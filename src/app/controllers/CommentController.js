8e5424f23ccd0701bb1feb2d01e2667a7e424e1a
Carlos Augusto <carlosass0.45@gmail.com>
2020-05-14 20:25:59 -0300

    Ajustando alguns detalhes da aplicação

--- src/app/controllers/CommentController.js
+++ src/app/controllers/CommentController.js
@@ -1,26 +1,45 @@
 import Comment from '../models/Comment';
+import Post from '../models/Post';
+import User from '../models/User';
+
+import Notification from '../schemas/Notification';
 
 class CommentController {
   async store(req, res) {
     const { content } = req.body;
     const { id } = req.params;
 
-    const comment = await Comment.create(
-      {
-        content,
-        post_id: id,
-        user_id: req.userId,
+    const post = await Post.findOne({
+      where: { id },
+      // include: [
+      //   {
+      //     model: User,
+      //     as: 'userpost',
+      //     attributes: ['name'],
+      //   },
+      // ],
+    });
+
+    const comment = await Comment.create({
+      content,
+      post_id: id,
+      user_id: req.userId,
+    });
+
+    // console.log(post);
+
+    const userComment = await User.findOne({
+      where: { id: req.userId },
+    });
+
+    if (comment) {
+      if (post.user_id !== req.userId) {
+        await Notification.create({
+          content: `${userComment.name}, comentou sua publicação`,
+          user_id: post.user_id,
+        });
       }
-      // {
-      //   include: [
-      //     {
-      //       model: User,
-      //       as: 'usercomment',
-      //       attributes: ['name', 'avatar'],
-      //     },
-      //   ],
-      // }
-    );
+    }
 
     return res.json(comment);
   }
